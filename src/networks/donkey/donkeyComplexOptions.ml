fd57e53d3b3d8ed3edb550f12bb75e4ff610bce9
spiralvoice <spiralvoice>
2005-06-25 01:07:05 +0000

    -

--- src/networks/donkey/donkeyComplexOptions.ml
+++ src/networks/donkey/donkeyComplexOptions.ml
@@ -629,7 +629,7 @@ let remove_server ip port =
 let config_files_loaded = ref false  
       
 let load _ =
-  lprintf "LOADING SHARED FILES AND SOURCES\n"; 
+  lprintf "EDK: Loading shared files\n"; 
   (try
       Options.load shared_files_ini;
       Options.load stats_ini;
