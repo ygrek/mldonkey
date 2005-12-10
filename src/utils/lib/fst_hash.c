0275baaac6291ac02b152ada462f5d912f1dc8be
spiralvoice <spiralvoice>
2005-12-10 19:44:28 +0000

    patch #4687

--- src/utils/lib/fst_hash.c
+++ src/utils/lib/fst_hash.c
@@ -181,9 +181,9 @@ static unsigned int fst_hash_small (unsigned char* data, unsigned int len, unsig
 #include "caml/mlvalues.h"
 
 /* returns checksum of fzhash */
-uint16 fst_hash_checksum (unsigned char *hash)
+unsigned short fst_hash_checksum (unsigned char *hash)
 {
-        uint16 sum = 0;
+        unsigned short sum = 0;
         int i;
 
         /* calculate 2 byte checksum used in the URL from 20 byte fthash */
