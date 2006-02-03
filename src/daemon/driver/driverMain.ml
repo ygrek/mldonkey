908b4136d686696c536803c7a7c5ced0cbc7f2d6
spiralvoice <spiralvoice>
2006-02-03 00:35:17 +0000

    patch #4857

--- src/daemon/driver/driverMain.ml
+++ src/daemon/driver/driverMain.ml
@@ -565,10 +565,12 @@ or getting a binary compiled with glibc %s.\n\n")
     (Sys.Signal_handle (fun _ -> lprintf_nl () "Received SIGTERM, stopping MLDonkey...";
         CommonInteractive.clean_exit 0));
 
+  if not Autoconf.windows then
   MlUnix.set_signal  Sys.sigusr1
     (Sys.Signal_handle (fun _ -> lprintf_nl () "Received SIGUSR1, saving options...";
         DriverInteractive.save_config ()));
 
+  if not Autoconf.windows then
   MlUnix.set_signal  Sys.sigusr2
     (Sys.Signal_handle (fun _ ->
 	lprintf_n () "Received SIGUSR2, starting garbage collection...";
