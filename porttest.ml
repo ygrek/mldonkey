9f8ec0cf8a284000fa8533e3b04d78fe2abcf8da
ygrek <ygrek@autistici.org>
2010-07-28 00:27:03 +0300

    done

--- porttest.ml
+++ porttest.ml
@@ -1,8 +1,7 @@
 
 open Printf
 open Bencode
-
-let url = sprintf "http://localhost/testport?port=%d"
+module BS = BasicSocket
 
 type network_porttest =
   PorttestNotAvailable
@@ -18,8 +17,6 @@ let get_user_agent () = "test"
 let porttest_result = ref PorttestNotStarted
 
 let pr fmt = Printf.ksprintf print_endline fmt
-let interpret_porttest s =
-  s
 
 let interpret_azureus_porttest s =
   let failure_message fmt = 
@@ -48,45 +45,48 @@ let interpret_azureus_porttest s =
   with _ ->
     failure_message "%s" "broken bencoded value"
 
-let start () =
-      let module H = Http_client in
-      azureus_porttest_random := (Random.int 100000);
-      porttest_result := PorttestInProgress (last_time ());
-      let r = {
-          H.basic_request with
-          H.req_url =
-            Url.of_string (Printf.sprintf
-              "http://azureus.aelitis.com/natcheck.php?port=%d&check=azureus_rand_%d"
-                !!client_port !azureus_porttest_random);
-(*           H.req_proxy = !CommonOptions.http_proxy; *)
-          H.req_user_agent = get_user_agent ();
-        } in
-      H.wget r (fun file ->
-        let result = interpret_azureus_porttest (File.to_string file) in
-        porttest_result := PorttestResult (last_time (), result)
-      )
+let interpret_utorrent_porttest s =
+  if String2.contains s "<div class=\"status-image\">OK!</div>" then
+    "OK"
+  else
+    "Port is not accessible"
 
-let start () =
+let start tests =
   let module H = Http_client in
+  match tests with
+  | [] -> porttest_result := PorttestResult (last_time(), "No tests available")
+  | _ ->
   porttest_result := PorttestInProgress (last_time ());
-  let r = {
+  let rec loop = function
+  | [] -> exit 1
+  | (url,interpret)::other ->
+    let r = {
       H.basic_request with
-      H.req_url = Url.of_string (url !client_port);
+      H.req_url = Url.of_string url;
       H.req_user_agent = get_user_agent ();
+      H.req_max_total_time = 30.;
     } in
-  pr "start";
-  H.wget_string r 
-    (fun s -> 
-      pr "got %d" (String.length s); 
-      porttest_result := PorttestResult (last_time (), interpret_porttest s); 
-      exit 0)
-    ~ferr:(fun code -> 
-      pr "error %d" code;
-      porttest_result := PorttestResult (last_time (), sprintf "Failed (%d)" code);
-      exit 1)
-    (fun _ _ -> pr "progress"; ())
+    pr "start %s" url;
+    H.wget_string r 
+      (fun s -> 
+        pr "got %d %s" (String.length s) (interpret s); 
+        porttest_result := PorttestResult (last_time (), interpret s);
+        exit 0)
+      ~ferr:(fun code -> 
+        pr "error %d" code;
+        porttest_result := PorttestResult (last_time (), sprintf "Remote service error (%d)" code);
+        loop other)
+      (fun _ _ -> ())
+  in
+  loop tests
 
 let () =
-  start ();
+  azureus_porttest_random := (Random.int 100000);
+  let tests = [
+    sprintf "http://azureus.aelitis.com/natcheck.php?port=%d&check=azureus_rand_%d"
+      !client_port !azureus_porttest_random, interpret_azureus_porttest;
+    sprintf "http://www.utorrent.com/testport?port=%d" !client_port, interpret_utorrent_porttest;
+  ] in
+  start tests;
   BasicSocket.loop ()
 
