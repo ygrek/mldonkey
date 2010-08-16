c14d5a25aaeb1a202a513025ceb4c2aac2d57bc7
spiralvoice <spiralvoice>
2010-08-16 19:19:23 +0000

    patch #7267

--- src/gtk2/gui/guiHtml.ml
+++ src/gtk2/gui/guiHtml.ml
@@ -50,9 +50,13 @@ let user_agent =
 
 let make_request url =
   let module H = Http_client in
+  let auth = match !!O.gtk_connection_http_proxy_login with
+  | "" -> None
+  | _ -> Some (!!O.gtk_connection_http_proxy_login, !!O.gtk_connection_http_proxy_password)
+  in
   let proxy =
     if !!O.gtk_connection_http_use_proxy
-      then Some (!!O.gtk_connection_http_proxy_server, !!O.gtk_connection_http_proxy_port)
+      then Some (!!O.gtk_connection_http_proxy_server, !!O.gtk_connection_http_proxy_port, auth)
       else None
   in
   let r = {
